9151f2e479678f647d50d050a775fe84a188e344
Kalpas123 <209745259@qq.com>
2025-12-27 19:15:51 +0800

    finished mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,93 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QMenu>
+#include <QAction>
+#include <QStatusBar>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("图书与借阅管理系统 v1.0");
+    this->setMinimumSize(800, 600); // 设置最小窗口尺寸
+
+    // 初始化数据库
+    if (!DatabaseManager::getInstance().initTables()) {
+        QMessageBox::critical(this, "致命错误", "数据库初始化失败！程序将退出。");
+        this->close();
+        return;
+    }
+
+    // 初始化子面板
+    m_bookPanel = new BookPanel(this);
+    m_readerPanel = new ReaderPanel(this);
+    m_borrowPanel = new BorrowPanel(this);
+
+    // 将子面板添加到堆叠窗口
+    ui->stackWidget->addWidget(m_bookPanel);
+    ui->stackWidget->addWidget(m_readerPanel);
+    ui->stackWidget->addWidget(m_borrowPanel);
+
+    // 初始化菜单栏
+    initMenuBar();
+
+    // 初始化状态栏
+    this->statusBar()->showMessage("就绪 - 当前用户：管理员", 3000);
+
+    // 默认选中第一个功能
+    ui->funcListWidget->setCurrentRow(0);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    // 子面板由parent析构，无需手动删除
+}
+
+void MainWindow::initMenuBar()
+{
+    // 1. 文件菜单
+    QMenu* fileMenu = this->menuBar()->addMenu("文件(&F)");
+    QAction* exportAction = new QAction("导出借阅记录(&E)", this);
+    QAction* exitAction = new QAction("退出(&X)", this);
+    fileMenu->addAction(exportAction);
+    fileMenu->addSeparator();
+    fileMenu->addAction(exitAction);
+
+    // 2. 帮助菜单
+    QMenu* helpMenu = this->menuBar()->addMenu("帮助(&H)");
+    QAction* aboutAction = new QAction("关于(&A)", this);
+    helpMenu->addAction(aboutAction);
+
+    // 绑定菜单事件
+    connect(exportAction, &QAction::triggered, this, [=]() {
+        m_borrowPanel->on_exportBorrowBtn_clicked(); // 调用借阅面板的导出功能
+    });
+    connect(exitAction, &QAction::triggered, this, &MainWindow::close);
+    connect(aboutAction, &QAction::triggered, this, [=]() {
+        QMessageBox::information(this, "关于", "图书与借阅管理系统\n基于Qt 5.15开发\n© 2025 课程设计");
+    });
+}
+
+void MainWindow::on_funcListWidget_currentRowChanged(int currentRow)
+{
+    // 切换堆叠窗口的当前面板
+    ui->stackWidget->setCurrentIndex(currentRow);
+
+    // 更新状态栏提示
+    switch (currentRow) {
+    case 0:
+        this->statusBar()->showMessage("当前模块：图书管理", 2000);
+        break;
+    case 1:
+        this->statusBar()->showMessage("当前模块：读者管理", 2000);
+        break;
+    case 2:
+        this->statusBar()->showMessage("当前模块：借阅管理", 2000);
+        break;
+    default:
+        break;
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,22 +2,36 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "bookpanel.h"
+#include "readerpanel.h"
+#include "borrowpanel.h"
 
-QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
-QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
+
+private slots:
+    // 左侧功能列表切换
+    void on_funcListWidget_currentRowChanged(int currentRow);
 
 private:
     Ui::MainWindow *ui;
+
+    // 子面板实例（由主窗口管理生命周期）
+    BookPanel* m_bookPanel;
+    ReaderPanel* m_readerPanel;
+    BorrowPanel* m_borrowPanel;
+
+    // 初始化菜单栏
+    void initMenuBar();
 };
+
 #endif // MAINWINDOW_H
